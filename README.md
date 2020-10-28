faea3e25b3bb710ec3c50c47efba195910cd780d
nadjaharold <rus0clov@gmail.com>
2020-10-28 18:26:12 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SampleFizzBuzz
+
+This repo is a practice of XMLHttpRequest.
+
+## Usage
+
+```bash
+node ./node/index.js
+```
---
+++ node/index.js
@@ -0,0 +1,48 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const endpoint = req.url;
+  if (endpoint === "/start") {
+    fs.readFile("./index.html", (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    });
+  }
+  if (endpoint === "/api") {
+    req.on("data", function (temp) {
+      let data = JSON.parse(temp);
+      const len = data.obj.length;
+
+      data2 = data.obj.slice();
+      data.obj = data2.map((val, key) => {
+        for (let i = 0; i < len; i++) {
+          if (key !== i && val.num % data2[i].num === 0) {
+            return { num: val.num, text: val.text + data2[i].text };
+          }
+        }
+        return { num: val.num, text: val.text };
+      });
+
+      let answer = { data: [] };
+      for (let i = 0; i < 30; i++) {
+        let isFizzBuzzLike = true;
+        for (let j = 0; j < len; j++) {
+          if ((i + 1) % data.obj[j].num === 0) {
+            isFizzBuzzLike = false;
+            answer.data[i] = data.obj[j].text;
+          }
+        }
+        if (isFizzBuzzLike) {
+          answer.data[i] = i + 1;
+        }
+      }
+
+      req.on("end", () => {
+        res.end(JSON.stringify(answer));
+      });
+    });
+  }
+});
+server.listen(8080);
